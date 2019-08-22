4e5826e8399328fea89c2fa8715a2021d1746fdc
TrueHumanSoul <truehumansoul@gmail.com>
2019-08-22 23:47:19 +0200

    UI Mass Update
    Create and Code UI, but game is not install inside. Need Feedback and answers about game integration.

---
+++ ZombieShow/Assets/Internal Assets/Scripts/UI/UIControllerScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerScript : MonoBehaviour
+{
+    [Header("Interfaces")]
+    public GameObject startInterface;
+    public GameObject shopInterface;
+    public GameObject aftermatchInterface;
+    public GameObject optionsInterface;
+    public GameObject upgradeInterface;
+    public GameObject gameInterface;
+    [Header("Upper Menu Links")]
+    public GameObject upperMenuShopLink;
+    public GameObject upperMenuOptionsLink;
+    public GameObject upperMenuUnknownLink;
+
+
+    public void Upgrade()
+    {
+        CloseAll();
+        CloseUpperLinksButNotPreferences();
+        upgradeInterface.SetActive(true);
+    }
+
+    public void Shop()
+    {
+        CloseAll();
+        shopInterface.SetActive(true);
+        CloseUpperLinksButNotPreferences();
+    }
+
+
+    public void Options()
+    {
+        CloseAll();
+        ShowUpperLinksButNotPreferences();
+        optionsInterface.SetActive(true);
+    }
+
+    public void ToMainMenu()
+    {
+        CloseAll();
+        ShowUpperLinksButNotPreferences();
+        startInterface.SetActive(true);
+    }
+
+    public void Game()
+    {
+        CloseAll();
+        CloseUpperLinksButNotPreferences();
+        gameInterface.SetActive(true);
+    }
+
+
+    public void Aftermatch()
+    {
+        CloseAll();
+        ShowUpperLinksButNotPreferences();
+        aftermatchInterface.SetActive(true);
+    }
+
+    public void StartAD()
+    {
+        Debug.Log("Commercial was Initialized");
+        Upgrade();
+    }
+
+    void CloseAll()
+    {
+        startInterface.SetActive(false);
+      shopInterface.SetActive(false);
+        aftermatchInterface.SetActive(false);
+        optionsInterface.SetActive(false);
+        upgradeInterface.SetActive(false);
+        gameInterface.SetActive(false);
+    }
+
+    void CloseUpperLinksButNotPreferences()
+    {
+        upperMenuShopLink.SetActive(false);
+        upperMenuUnknownLink.SetActive(false);
+    }
+
+
+    void ShowUpperLinksButNotPreferences()
+    {
+        upperMenuShopLink.SetActive(true);
+        upperMenuUnknownLink.SetActive(true);
+    }
+
+
+}
